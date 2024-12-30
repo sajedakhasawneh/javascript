47b01c571b160fbaafa5bc17e222201b6cc45eb5
sajedakhasawneh <sajedakhasawneh5@gmail.com>
2024-12-30 22:29:10 +0300

    task

---
+++ 30-12-2024/test.js
@@ -0,0 +1,152 @@
+//task
+
+// const zakat=0.025;
+// let zakatValue = 0;
+// let x =250;
+// if(x >= 4500){
+//     zakatValue= x*zakat;
+//     console.log("the zakat value is "+ zakatValue);
+// }
+// else{
+//     console.log("No zakat for this value.")
+// }
+
+
+
+// task1
+
+//1  [ 1,7  9  45, ]     -->  [1,7,9,45]
+
+//2 ["Str" "alex","moh"
+
+//  'the','fox' 'over' lazy, 'dog',  ]  -->  ["Str", "alex", "moh", "the", "fox", "over", "lazy" , "dog"]
+
+
+
+// task2
+ // "Banana"  --> 1
+ //"Tomato"  --> 0
+ 
+ 
+// task3
+
+// let food= ["",""];
+// let sport = ["", "", ""];
+// let movie = ["", ""];
+
+
+//task4
+
+// let firstOfArray = [1,4,5];
+// let firstnum = "";
+// for(let i =0; i<firstOfArray.length; i++){
+//     firstnum = firstOfArray[i];
+//     break;}
+// console.log(firstnum);
+
+
+
+
+
+
+//task5
+
+// let lastOfArray = [1,4,5,10,50,60,70]
+// let lastnum = "";
+// for(let i = lastOfArray.length-1; i<lastOfArray.length; i--){
+//     lastnum = lastOfArray[i];
+//     break;}
+// console.log(lastnum);
+
+
+
+
+
+// //task6
+
+// var arr=[0,5,7,9];
+// arr.shift();
+// arr.shift();
+// arr.shift();
+// arr.unshift(1,3,4,6,8);
+// arr.push(10);
+// console.log(arr);
+
+
+
+
+
+//task7
+    // var array2=[5,9,-7,3.5];
+    // array2.push(10, 15);
+    // console.log(array2);
+    // array2.pop();
+    // console.log(array2);
+    // array2.shift();
+    // console.log(array2);
+    // array2.unshift(1,3);
+    // console.log(array2);
+
+
+
+
+
+//task8
+    // let array1= [-3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+    // let sortarr=[];
+    // let min=0;
+    // let max = 100;
+    // for(let i =0; i<array1.length; i++){
+    //     // min = array1[0];
+    //     if(array1[i] <  min)
+    //        min = array1[i];
+    //     // console.log(array1[i]);
+    // }
+    // array1.sort((x ,y) => x - y);
+    // console.log(array1);
+    
+
+
+
+// task9
+
+// function rateFunction(x) {
+//     if( x >= 90 && x <=100)
+//         console.log("A");
+
+//     if( x >= 80 && x <=89)
+//         console.log("B");
+
+//     if( x >= 70 && x <=79)
+//         console.log("C");
+
+//     if( x >= 60 && x <=69)
+//         console.log("D");
+
+//     if( x >= 50 && x <=59)
+//         console.log("E");
+
+//     if( x < 50)
+//         console.log("Fail");
+
+// }
+
+// rateFunction(45);
+
+
+
+
+//task10 
+let num = 8;
+for (let i=1; i<= num; i++){
+     let x = "";
+     for( let j=1; j<= i ;j++) 
+        x += j + " ";
+        console.log(x);    
+}
+
+
+// for(let i=1;i<=8; i++)
+//     for(let j=1; j<=i; j++)
+//         console.log(j);
+        
