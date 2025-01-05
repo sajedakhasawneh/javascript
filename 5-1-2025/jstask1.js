abcc50b48f26bbf6e641438f25c09594e702052f
sajedakhasawneh <sajedakhasawneh5@gmail.com>
2025-01-06 01:39:08 +0300

    text

---
+++ 5-1-2025/jstask1.js
@@ -0,0 +1,10 @@
+function savetolocalstorage(){
+    localStorage.setItem("", document.getElementById("name").value);
+    localStorage.setItem("", document.getElementById("age").value);
+    localStorage.setItem("", document.getElementById("gender").value);
+    localStorage.setItem("", document.getElementById("date").value);
+    // localStorage.setItem("", document.getElementById("name").value);//img
+    localStorage.setItem("", document.getElementById("desc").value);
+    localStorage.setItem("", document.getElementById("major").value);
+    localStorage.setItem("", document.getElementById("name").value);
+}
